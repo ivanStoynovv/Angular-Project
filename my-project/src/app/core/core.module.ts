3f1d849103e7482ec802308197fa1e7783eec556
ivanStoynovv <igs9503@gmail.com>
2024-03-19 23:24:07 +0200

    Render header and nav bar

--- my-project/src/app/core/core.module.ts
+++ my-project/src/app/core/core.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { MainComponent } from './main/main.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 
 
@@ -11,10 +12,11 @@ import { MainComponent } from './main/main.component';
     HeaderComponent,
     FooterComponent,
     MainComponent,
+    NavBarComponent,
   ],
   imports: [
     CommonModule,
   ],
-  exports: [FooterComponent, HeaderComponent, MainComponent]
+  exports: [FooterComponent, HeaderComponent, MainComponent, NavBarComponent]
 })
 export class CoreModule { }
